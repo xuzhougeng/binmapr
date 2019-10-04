d762bfafc5960bd7a2db3a4bd92ff5075866719b
xuzhougeng <18868192325m0@sina.cn>
2019-10-04 18:37:08 +0800

    add max.depth

--- R/callGtFromAd.R
+++ R/callGtFromAd.R
@@ -3,14 +3,18 @@
 #' @param x AD matrix
 #' @param  min.depth minimum depth to infer the genotype, if
 #' depth lower than it, it will be conside as NA
+#' @param  max.depth maximum depth to infer the genotype, if
+#' depth larger than it, it will be conside as NA
 #' @param low  threshold to infer one parent, 0
 #' @param high threshold to infer another parent, 2
 #'
 #' @export
 #' @author Zhou-geng Xu
-callGtFromAd <- function(x, min.depth = 10, low = 0.2, high = 0.8){
+callGtFromAd <- function(x, min.depth = 10, max.depth = 200, 
+						 low = 0.2, high = 0.8){
 
-  freq_mt <- calcFreqFromAd(x, min.depth = min.depth)
+  freq_mt <- calcFreqFromAd(x, min.depth = min.depth,
+                               max.depth = max.depth)
 
   GT_mt <- ifelse(freq_mt < low, 0, ifelse(freq_mt > high, 2 , 1) )
 
@@ -22,10 +26,12 @@ callGtFromAd <- function(x, min.depth = 10, low = 0.2, high = 0.8){
 #' @param x AD matrix
 #' @param min.depth minimum depth to infer the genotype, if
 #' depth lower than it, it will be conside as NA
+#' @param  max.depth maximum depth to infer the genotype, if
+#' depth larger than it, it will be conside as NA
 #'
 #' @export
 #' @author Zhou-geng Xu
-calcFreqFromAd <- function(x, min.depth = 10){
+calcFreqFromAd <- function(x, min.depth = 10, max.depth = 200){
 
   # fix the bug that AD is NA
   x[which(is.na(x))] <- "0,0"
@@ -37,7 +43,7 @@ calcFreqFromAd <- function(x, min.depth = 10){
   Alt_COUNT <- AD_COUNT[seq.int(2,length(AD_COUNT),2)]
   AD_Depth <- Ref_COUNT + Alt_COUNT
 
-  NA_pos <- which(AD_Depth < min.depth)
+  NA_pos <- which(AD_Depth < min.depth | AD_Depth > max.depth)
   Ref_COUNT[NA_pos] <- NA
   Alt_COUNT[NA_pos] <- NA
 
