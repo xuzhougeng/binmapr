d4cfa5e31e0f81838cad6b610ea11c9b635f53bf
xuzhougeng <18868192325m0@sina.cn>
2019-10-06 22:59:30 +0800

    add pop.type function to buildMstmapInput

--- R/buildMstmapInput.R
+++ R/buildMstmapInput.R
@@ -1,6 +1,11 @@
 #' Generate input for mstmap from callWindowGeno result
 #'
 #' @param geno gentype matrix or list from callWindowGeno
+#' @param pop.type Character string specifying the population
+#' type of the data frame object. Accepted values are "DH"
+#' (doubled haploid), "BC" (backcross), "RILn" (non-advanced
+#' RIL population with n generations of selfing) and "ARIL"
+#' (advanced RIL) (see Details). Default is "DH".
 #'
 #' @return data.frame
 #' @examples
@@ -11,7 +16,7 @@
 #'
 #' @export
 #' @author Zhougeng Xu
-buildMstmapInput <- function(geno){
+buildMstmapInput <- function(geno, pop.type = "DH"){
 
   if ( inherits(geno, "matrix")){
     mt <- geno
@@ -21,10 +26,31 @@ buildMstmapInput <- function(geno){
     stop("input should be matrix or output from batchCallGeno")
   }
 
-  mt[mt == 0] <- "A"
-  mt[mt == 2] <- "B"
-  mt[mt == 1] <- "X"
-  mt[is.na(mt)] <- "U"
+  # encode the genotype
+  if (pop.type == "DH"){
+    mt[mt == 0] <- "A"
+    mt[mt == 2] <- "B"
+    mt[mt == 1] <- "U"
+    mt[is.na(mt)] <- "U"
+  } else if (pop.type == "BC"){
+    mt[mt == 0] <- "A"
+    mt[mt == 2] <- "U"
+    mt[mt == 1] <- "B"
+    mt[is.na(mt)] <- "U"
+  } else if (pop.type == "RILn"){
+    mt[mt == 0] <- "A"
+    mt[mt == 2] <- "B"
+    mt[mt == 1] <- "X"
+    mt[is.na(mt)] <- "U"
+  } else if(pop.type == "ARIL"){
+    mt[mt == 0] <- "A"
+    mt[mt == 2] <- "B"
+    mt[mt == 1] <- "U"
+    mt[is.na(mt)] <- "U"
+  } else{
+    stop("unknown popultaion type, only DH, BC, RILn, ARIL is allowed")
+  }
+
   df <- cbind.data.frame(mt, stringsAsFactors = FALSE)
   return(df)
 
